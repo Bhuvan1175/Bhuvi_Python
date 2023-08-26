e9042fe7fa0159eed3d7b194aa9c6d5cceaaf5b0
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2023-08-26 22:02:19 +0530

    Add files via upload

---
+++ 23_Calculator_Small_Application.py
@@ -0,0 +1,35 @@
+ #Design a calculator
+#Addition operation
+def add():
+    x=int(input("Enter first number\t:"))
+    y=int(input("Enter second number\t:"))
+    z=x+y
+    print("Addition")
+    print(z)
+add()
+#Subtraction
+def sub():
+    x=int(input("Enter first number\t:"))
+    y=int(input("Enter second number\t:"))
+    a=x-y
+    print("Subtraction")
+    print(a)
+sub()
+
+
+#Multiplication
+def mul():
+    x=int(input("Enter first number\t:"))
+    y=int(input("Enter second number\t:"))
+    b=x*y
+    print("Multiplication")
+    print(b)
+mul()
+#Division
+def divide():
+    x=int(input("Enter first number\t:"))
+    y=int(input("Enter second number\t:"))
+    c=x/y
+    print("Division")
+    print(c)
+divide()
